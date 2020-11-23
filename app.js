e6606e64edb145f70c7958a61fd1d280d6723192
PeaWarrior <jacksonchen171@gmail.com>
2020-11-23 13:30:23 -0500

    initialized yarg commands

--- app.js
+++ app.js
@@ -1,5 +1,57 @@
-// in order to use a module, you must require it
-const getNotes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes');
 
-console.log(chalk.green('Success!'));
+// customize yargs version
+yargs.version('1.1.0');
+
+// add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    }
+  },
+  handler: function(params){
+    console.log(chalk.blue('Title: ' + params.title));
+    console.log(chalk.blue('Body: ' + params.body));
+  }
+});
+
+// remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function() {
+    console.log(chalk.blue('Removing note'));
+  }
+});
+
+// list
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler: function() {
+    console.log(chalk.blue('Listing all notes'));
+  }
+});
+
+// read
+yargs.command({
+  command: 'read',
+  decribe: 'Read note',
+  handler: function() {
+    console.log(chalk.blue('Reading note'));
+  }
+});
+
+yargs.parse();
